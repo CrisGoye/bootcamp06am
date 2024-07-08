c29d252b1604b989864e4e64a297fa869e0ec6c4
Cristian Goyeneche <markaymarka.publicidad@hotmail.com>
2024-07-08 06:54:50 -0500

    cambio stylos

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+const datos = {
+    nombre: "",
+    celular: "",
+    correo: "" ,
+    mensaje: ""
+}
+
+//Evento de los input
+const nombre = document.querySelector("#nombre");
+const celular = document.querySelector("#celular");
+const correo = document.querySelector("#correo");
+const mensaje = document.querySelector("#mensaje");
+//console.log(mensaje);
+
+//prueba EventListener
+//nombre.addEventListener('input', prueba);
+//function prueba(){
+    //alert('ingresaste el nombre');
+//}
+
+nombre.addEventListener('input', leerTexto);
+celular.addEventListener('input', leerTexto);
+correo.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(evento){
+    //console.log(evento.target.value);
+    datos[evento.target.id] = evento.target.value;
+    //console.log(datos);
+}
+
+const formulario = document.querySelector('.formulario');
+//console.log(formulario);
+
+formulario.addEventListener('submit',function(evento){
+    evento.preventDefault();
+    //---> destructuring
+    const {nombre, celular, correo, mensaje} = datos;
+    //console.log(nombre, celular, correo, mensaje);
+        if(nombre === ''|| celular === ''|| correo === ''|| mensaje === ''){
+
+     mostrarError('Diligencia todos los campos');
+     return; //---> corta la ejecucion del codigo   
+    }   
+
+ function mostrarError(mensaje){
+    //console.log(mensaje);
+    let error = document.createElement('p'); <p></p>
+    error.textContent = mensaje; <p></p>
+    error.classList.add('error');
+    //console.log(error);
+    formulario.appendChild(error);
+} 
+
+
+})
+
+
+
+
